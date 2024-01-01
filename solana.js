4a2361956895735d005084aaedf46ebbcbd8c308
Alisa Makarova <alisa@paytweed.com>
2024-01-01 12:58:30 +0200

    update connection instance and add try catch to functions

--- solana.js
+++ solana.js
@@ -1,62 +1,106 @@
-const web3 =  require('@solana/web3.js');
-const bip39 =  require("bip39");
-const splToken = require('@solana/spl-token');
+const web3 = require("@solana/web3.js");
+const bip39 = require("bip39");
+const splToken = require("@solana/spl-token");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const mnemonic = "trouble sport ignore faint hidden mushroom van future naive spike issue sheriff"
+const mnemonic =
+  "trouble sport ignore faint hidden mushroom van future naive spike issue sheriff";
 
 function getUserAccount() {
-    const NO_PASSWORD = "";
-    const seed = bip39.mnemonicToSeedSync(mnemonic, NO_PASSWORD); 
-    const keypair = web3.Keypair.fromSeed(seed.slice(0, 32));
-    console.log(`User Account: https://explorer.solana.com/address/${keypair.publicKey.toBase58()}?cluster=devnet}`);
-    return keypair;
+  const NO_PASSWORD = "";
+  const seed = bip39.mnemonicToSeedSync(mnemonic, NO_PASSWORD);
+  const keypair = web3.Keypair.fromSeed(seed.slice(0, 32));
+  console.log(
+    `User Account: https://explorer.solana.com/address/${keypair.publicKey.toBase58()}?cluster=devnet}`
+  );
+  return keypair;
 }
 
 async function airdropSolToUserAccount(keypair, connection) {
-    try {
-        await connection.requestAirdrop(keypair.publicKey, 1000000000);
-    } catch (error) {
-        console.log(`Could not airdrop: ${error}`);
-    }
+  try {
+    await connection.requestAirdrop(keypair.publicKey, 1000000000);
+  } catch (error) {
+    console.log(`Could not airdrop: ${error}`);
+  }
 }
 
 async function createMintAccount(connection, keypair) {
-    const decimals = 8;
-    const publicKey = await splToken.createMint(connection, keypair, keypair.publicKey, keypair.publicKey, decimals);
-    console.log(`Token Mint Account: https://explorer.solana.com/address/${publicKey.toBase58()}?cluster=devnet`);
-    return publicKey;
+  const decimals = 8;
+  const publicKey = await splToken.createMint(
+    connection,
+    keypair,
+    keypair.publicKey,
+    keypair.publicKey,
+    decimals
+  );
+  console.log(
+    `Token Mint Account: https://explorer.solana.com/address/${publicKey.toBase58()}?cluster=devnet`
+  );
+  return publicKey;
 }
 
 async function createTokenAccount(connection, mint, keypair) {
-    const account_address = await splToken.createAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
-    console.log(`Token Account: https://explorer.solana.com/address/${account_address}?cluster=devnet`);
+  try {
+    const account_address = await splToken.createAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+    console.log(
+      `Token Account: https://explorer.solana.com/address/${account_address}?cluster=devnet`
+    );
     return account_address;
+  } catch (error) {
+    console.log(`createTokenAccount error: ${error.message}`);
+    throw error;
+  }
 }
 
 async function mint(connection, keypair, mint, tokenAccount) {
-    console.log(`${connection} ${keypair} ${mint} ${tokenAccount}`);
-    const transactionSignature = await splToken.mintTo(connection, keypair, mint, tokenAccount, keypair, 1);
+  try {
+    // console.log(`${connection} ${keypair} ${mint} ${tokenAccount}`);
+    const transactionSignature = await splToken.mintTo(
+      connection,
+      keypair,
+      mint,
+      tokenAccount,
+      keypair,
+      1
+    );
     console.log(`Transaction hash: ${transactionSignature}`);
     return transactionSignature;
+  } catch (error) {
+    console.log(`mint error: ${error.message}`);
+  }
 }
 
 (async () => {
-    const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
-    const keypair = getUserAccount();
-    await airdropSolToUserAccount(keypair, connection);
-    const mintAccountPubkey = await createMintAccount(connection, keypair);
-    const tokenAccountAddress = await createTokenAccount(connection, mintAccountPubkey, keypair);
-    const transactionHash = await mint(connection, keypair, mintAccountPubkey, tokenAccountAddress);
-    
+  const connection = new web3.Connection(
+    web3.clusterApiUrl("devnet"),
+    "confirmed"
+  );
+  const keypair = getUserAccount();
+  //await airdropSolToUserAccount(keypair, connection);
+  const mintAccountPubkey = await createMintAccount(connection, keypair);
+  const tokenAccountAddress = await createTokenAccount(
+    connection,
+    mintAccountPubkey,
+    keypair
+  );
+  const transactionHash = await mint(
+    connection,
+    keypair,
+    mintAccountPubkey,
+    tokenAccountAddress
+  );
 
-    // const number_of_tokens = 1 * Math.pow(10, MINT_CONFIG.numDecimals)
-    // let mint = await splToken.createMintToInstruction(
-    //     mintKeypair.publicKey, 
-    //     mintAuthority, 
-    //     tokenAccountAddress, 
-    //     number_of_tokens);
-
-
+  //   const number_of_tokens = 1 * Math.pow(10, MINT_CONFIG.numDecimals);
+  //   let mint = await splToken.createMintToInstruction(
+  //     mintKeypair.publicKey,
+  //     mintAuthority,
+  //     tokenAccountAddress,
+  //     number_of_tokens
+  //   );
 })();
