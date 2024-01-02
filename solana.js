0e212f2960fa24569d34f141209047c1e1f41453
Alisa Makarova <alisa@paytweed.com>
2024-01-02 17:37:23 +0200

    implement mintAndTransferNFT function

--- solana.js
+++ solana.js
@@ -1,20 +1,18 @@
 import {
   Connection,
   Keypair,
-  PublicKey,
   clusterApiUrl,
   Transaction,
   sendAndConfirmTransaction,
+  PublicKey,
 } from "@solana/web3.js";
-import { mnemonicToSeedSync } from "bip39";
+import { mnemonicToSeedSync, generateMnemonic } from "bip39";
 import {
-  AuthorityType,
-  createSetAuthorityInstruction,
   createMint,
-  getOrCreateAssociatedTokenAccount,
   createAssociatedTokenAccount,
   mintTo,
-  createMintToInstruction,
+  transferChecked,
+  closeAccount,
 } from "@solana/spl-token";
 import {
   Metaplex,
@@ -24,12 +22,13 @@ import {
 
 // require("dotenv").config();
 
-const mnemonic =
+const DEFAULT_MNEMONIC =
   "trouble sport ignore faint hidden mushroom van future naive spike issue sheriff";
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-function getUserAccount() {
+function getUserAccount(mnemonic) {
   const NO_PASSWORD = "";
-  const seed = mnemonicToSeedSync(mnemonic, NO_PASSWORD);
+  const seed = mnemonicToSeedSync(mnemonic || DEFAULT_MNEMONIC, NO_PASSWORD);
   const keypair = Keypair.fromSeed(seed.slice(0, 32));
   console.log(
     `User Account: https://explorer.solana.com/address/${keypair.publicKey.toBase58()}?cluster=devnet}`
@@ -126,63 +125,11 @@ async function mint(connection, keypair, mint, tokenAccount) {
 //   //   );
 // })();
 
-async function createNft() {
-  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-  const payerKeypair = getUserAccount();
-
-  const mint = await createMint(
-    connection,
-    payerKeypair,
-    payerKeypair.publicKey,
-    payerKeypair.publicKey,
-    0
-  );
-
-  const associatedTokenAccount = await getOrCreateAssociatedTokenAccount(
-    connection,
-    payerKeypair,
-    mint,
-    payerKeypair.publicKey
-  );
-
-  await mintTo(
-    connection,
-    payerKeypair,
-    mint,
-    associatedTokenAccount.address,
-    payerKeypair,
-    1
-  );
-
-  let transaction = new Transaction().add(
-    createSetAuthorityInstruction(
-      mint,
-      payerKeypair.publicKey,
-      AuthorityType.MintTokens,
-      null
-    )
-  );
-
-  const transactionSignature = await sendAndConfirmTransaction(
-    connection,
-    transaction,
-    [payerKeypair]
-  );
-
-  console.log(
-    `Transaction hash: https://solscan.io/tx/${transactionSignature}?cluster=devnet`
-  );
-}
-
 // mints directly to the wallet
-async function createNftWithMetadata() {
+async function createNftWithMetadata(payerKeypair) {
   try {
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-    const payerKeypair = getUserAccount();
-
     const metaplex = Metaplex.make(connection)
-      .use(keypairIdentity(payerKeypair))
+      .use(keypairIdentity(payerKeypair || getUserAccount()))
       .use(bundlrStorage({ address: "https://devnet.bundlr.network" }));
 
     const { uri } = await metaplex.nfts().uploadMetadata({
@@ -192,18 +139,113 @@ async function createNftWithMetadata() {
         "https://static.wixstatic.com/media/0fe759_eed11ea2e1c240b1847f0cfa80b9290b~mv2.png/v1/fill/w_260,h_294,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/%D7%A1%D7%91%D7%AA%D7%90%20%D7%A4%D7%99%D7%91%D7%99.png",
     });
 
-    const { nft } = await metaplex.nfts().create({
-      name: "Fibi's second NFT",
+    const res = await metaplex.nfts().create({
+      name: "Fibi's first NFT",
       uri,
     });
 
     console.log(`   Success!🎉`);
     console.log(
-      `   Minted NFT: https://explorer.solana.com/address/${nft.address}?cluster=devnet`
+      `   Minted NFT: https://explorer.solana.com/address/${res.nft.address}?cluster=devnet`
     );
+
+    return {
+      nftAddress: res.nft.address,
+      tokenAccountAddress: res.tokenAddress,
+    };
   } catch (error) {
     console.log({ error });
   }
 }
 
-createNftWithMetadata();
+function generateSolanaKeypair() {
+  const mnemonic = generateMnemonic();
+  console.log("Mnemonic:", mnemonic);
+
+  const seed = mnemonicToSeedSync(mnemonic, "");
+  const keypair = Keypair.fromSeed(seed.slice(0, 32));
+
+  console.log("Public Key:", keypair.publicKey.toBase58());
+
+  return keypair;
+}
+
+// async function main() {
+//   const keypair = generateSolanaKeypair();
+//   const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+//   await airdropSolToUserAccount(keypair, connection);
+//   await createNftWithMetadata(connection, keypair);
+// }
+
+async function generateTokenAccount(nftAddress, ownerWallet, payerWallet) {
+  const ata = await createAssociatedTokenAccount(
+    connection,
+    payerWallet,
+    nftAddress, // mint (nft)
+    ownerWallet.publicKey // owner of created ata,
+  );
+
+  return ata;
+}
+
+async function mintAndTransferNFT() {
+  const senderKeypair = getUserAccount(
+    "erosion bean muscle feel clay order spend mammal yard tooth balcony surround"
+  );
+  // will mint nft to the sender wallet
+  const { nftAddress, tokenAccountAddress: sourceTokenAccountAddress } =
+    await createNftWithMetadata(senderKeypair);
+  const receiverWallet = getUserAccount();
+  // create token account for receiver
+  const receiverTokenAccount = await generateTokenAccount(
+    nftAddress,
+    receiverWallet,
+    senderKeypair
+  );
+
+  try {
+    const transferTxHash = await transferChecked(
+      connection,
+      senderKeypair, // payer
+      sourceTokenAccountAddress, // from (should be a token account)
+      nftAddress, // mint (nft)
+      receiverTokenAccount, // to (should be a token account)
+      senderKeypair, // from's owner
+      1, // amount, if your deciamls is 8, send 10^8 for 1 token
+      0 // decimals
+    );
+
+    console.log("-------------------------------------------------");
+
+    console.log(
+      `   Successfully transfered nft ${nftAddress} to token account ${receiverTokenAccount}!🎉`
+    );
+    console.log(
+      `   Transaction: https://explorer.solana.com/tx/${transferTxHash}?cluster=devnet`
+    );
+    console.log("-------------------------------------------------");
+
+    // close source token account address after the transfer
+    const accCloseTxHash = await closeAccount(
+      connection,
+      senderKeypair, // payer
+      sourceTokenAccountAddress, // token account which you want to close
+      senderKeypair.publicKey, // destination
+      senderKeypair // owner of token account
+    );
+
+    console.log("-------------------------------------------------");
+    console.log(
+      `   Successfully closed token account ${sourceTokenAccountAddress}!🎉`
+    );
+    console.log(
+      `   Transaction: https://explorer.solana.com/tx/${accCloseTxHash}?cluster=devnet`
+    );
+    console.log("-------------------------------------------------");
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+mintAndTransferNFT();
